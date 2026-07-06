3cc9498124097f99afeb3c4bea1f19ef742e68ca
Alex <gatseliuk.alex@gmail.com>
2026-07-06 19:52:57 +0200

    wasup

--- EDA_1.py
+++ EDA_1.py
@@ -2,4 +2,11 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from scipy import stats
+import seaborn as sns
+
+df = sns.load_dataset('titanic')
+
+print(df.head)
+print(df.info)
+print(df.axis[0], df.axis[1])
 
