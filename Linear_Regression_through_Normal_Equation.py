416399e5487e3ae9fa36a563600663f06c3db656
Alex <gatseliuk.alex@gmail.com>
2026-07-13 11:55:33 +0200

    mama

--- Linear_Regression_through_Normal_Equation.py
+++ Linear_Regression_through_Normal_Equation.py
@@ -1,8 +1,27 @@
+from sklearn.linear_model import LinearRegression
 import numpy as np
 
-X = np.array([[1, 2, 3, 4, 5, 6, 7, 8], 
-             [1, 1, 1, 1, 1, 1, 1, 1]])
-y = np.array([52, 58, 62, 68, 74, 78, 84, 88])
+X = np.array([[1, 1],
+              [1, 2], 
+              [1, 3],
+              [1, 4],
+              [1, 5],
+              [1, 6],
+              [1, 7],
+              [1, 8]])
 
-print(X.T)
-print(X.shape)
+y = np.array([52, 58, 62, 68, 74, 78, 84, 88])
+# w = (X^T X)^{-1} X^T y
+def matrix_formula():
+    m_to_inv = X.T @ X
+    inverted_X = np.linalg.inv(m_to_inv)
+    transponded_X = X.T @ y
+    res = inverted_X @ transponded_X
+    print(res)
+    
+
+matrix_formula()
+
+reg = LinearRegression().fit(X, y)
+
+print(reg)
