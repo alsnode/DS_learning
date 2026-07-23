5d9d4f2aa3f2e2811cc5d2f82f06c3ebc3e6552a
Alex <gatseliuk.alex@gmail.com>
2026-07-23 18:54:26 +0200

    hope this works

--- ex_3.4.2.py
+++ ex_3.4.2.py
@@ -12,12 +12,22 @@ df = pd.DataFrame({
     'num_sessions': np.random.poisson(15, n),
     'referral_source': np.random.choice(['organic','ads','referral', None], n, p=[0.4,0.3,0.2,0.1]),
     'country': np.random.choice(['US','UK','DE','FR','ES'], n),
-    'days_since_last_login': np.datetime64('now') - pd.to_timedelta(np.random.exponential(30,n)),                                                                                             
-})
+    'days_since_last_login': np.datetime64('now') - pd.to_timedelta(np.random.exponential(30,n))
+    })
+
 # добавим немного грязи
 df.loc[np.random.choice(n, 20), 'total_spent'] = np.nan
 df.loc[np.random.choice(n, 15), 'num_sessions'] = -1# ошибка в данных
 
-account_age_days = np.busday_count(df['signup_date'], np.datetime64('today'), unit='D')
-df.append(account_age_days)  
+
+for i in df['num_sessions']:
+    if i / 1 <= 0:
+        df[df.num_sessions]
+    else:
+        continue
+
+
+
+
+    
 
