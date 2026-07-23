bb87d8582818be2eb59a65b03d9c67dec539e391
Alex <gatseliuk.alex@gmail.com>
2026-07-23 20:16:59 +0200

    update

---
+++ ex_3.4.2_copy.py
@@ -0,0 +1,32 @@
+import numpy as np 
+import pandas as pd
+
+
+np.random.seed(5)
+n = 500
+df = pd.DataFrame({
+    'user_id': range(n),
+    'signup_date': pd.to_datetime('2023-01-01') + pd.to_timedelta(np.random.randint(0, 700, n), unit='D'),
+    'last_login': pd.to_datetime('2024-06-01') - pd.to_timedelta(np.random.exponential(30, n)),
+    'total_spent': np.random.exponential(200, n),
+    'num_sessions': np.random.poisson(15, n),
+    'referral_source': np.random.choice(['organic','ads','referral', None], n, p=[0.4,0.3,0.2,0.1]),
+    'country': np.random.choice(['US','UK','DE','FR','ES'], n),
+    'days_since_last_login': np.datetime64('now') - pd.to_timedelta(np.random.exponential(30,n))
+    })
+
+# добавим немного грязи
+df.loc[np.random.choice(n, 20), 'total_spent'] = np.nan
+df.loc[np.random.choice(n, 15), 'num_sessions'] = -1# ошибка в данных
+
+
+for i in df['num_sessions']:
+    if i / 1 <= 0:
+        df.at[df[i].index, 'num_sessions'] = 0
+    else:
+        continue
+
+
+
+
+    
